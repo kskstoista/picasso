1b2e592e9bd69df603e1e9cace93ef826eedbef8
Kakstoista <kakstoista.github@gmail.com>
2013-04-23 19:26:30 +0300

    Added: Album list Pictures in Album Single piture view

--- gen/com/my/picasso/R.java
+++ gen/com/my/picasso/R.java
@@ -14,18 +14,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int listDescription=0x7f070003;
+        public static final int listImage=0x7f070001;
+        public static final int listTitle=0x7f070002;
+        public static final int menu_settings=0x7f070004;
+        public static final int picasa_image=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_picasa_main=0x7f030000;
+        public static final int activity_picasa_album=0x7f030000;
+        public static final int activity_picasa_image_view=0x7f030001;
+        public static final int activity_picasa_main=0x7f030002;
+        public static final int list_item_layout=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_picasa_main=0x7f060000;
+        public static final int activity_picasa_album=0x7f060000;
+        public static final int activity_picasa_image_view=0x7f060001;
+        public static final int activity_picasa_main=0x7f060002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int title_activity_picasa_album=0x7f040003;
+        public static final int title_activity_picasa_image_view=0x7f040004;
     }
     public static final class style {
         /** 
--- src/com/my/picasso/PicasaMain.java
+++ src/com/my/picasso/PicasaMain.java
@@ -1,17 +1,125 @@
 package com.my.picasso;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
-public class PicasaMain extends Activity {
+import org.json.*;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.my.picasso.AlbumListDownloader;
+
+public class PicasaMain extends ListActivity {
+
+	private AlbumListDownloader ad;
+	private String[] albumNames;
+	private String[] albumIds;
+	private String googleKey;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_picasa_main);
+		 googleKey = getAuthString();
+		
+        if(googleKey != ""){
+        	ad = new AlbumListDownloader(this);
+        	ad.execute(googleKey);
+        	
+            Log.d("Main","Oncreate finish");
+        }
+}
+	
+	 public String[] getAlbumNames() {
+		return albumNames;
 	}
 
+	public void setAlbumNames(String[] albumNames) {
+		this.albumNames = albumNames;
+	}
+
+	public String[] getAlbumIds() {
+		return albumIds;
+	}
+
+	public void setAlbumIds(String[] albumIds) {
+		this.albumIds = albumIds;
+	}
+
+	@Override
+	 protected void onListItemClick(ListView l, View v, int position, long id) {
+		 Toast wrongToast = Toast.makeText(this, "Clicked "+albumNames[position]+" ID:"+albumIds[position], Toast.LENGTH_SHORT);
+			wrongToast.show();
+         Intent startAlbum = new Intent(this,PicasaAlbum.class);
+         startAlbum.putExtra("ALBUM_NAME",albumNames[position] );
+         startAlbum.putExtra("ALBUM_ID",albumIds[position] );
+         startAlbum.putExtra("AUTH",googleKey );
+         startActivity(startAlbum);
+	 }
+	
+	public String getAuthString(){
+	    String googleKey = new String();
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpPost httppost = new HttpPost("https://www.google.com/accounts/ClientLogin");
+
+        List<NameValuePair> loginValues = new ArrayList<NameValuePair>(2);
+        loginValues.add(new BasicNameValuePair("accountType", "GOOGLE"));
+        loginValues.add(new BasicNameValuePair("Email", "ferenc.muskatal@gmail.com"));
+        loginValues.add(new BasicNameValuePair("Passwd", "evogsnx1.pac"));
+        loginValues.add(new BasicNameValuePair("service", "lh2"));
+        loginValues.add(new BasicNameValuePair("source", "Picasso"));
+        try{
+            httppost.setEntity(new UrlEncodedFormEntity(loginValues));
+		    HttpResponse response = httpclient.execute(httppost);
+    	
+	        InputStream responseStream = null;
+    	    BufferedReader responseReader = null;
+			
+	        if(response.getStatusLine().getStatusCode() == HttpStatus.SC_OK ){
+                try {
+	   		        responseStream = response.getEntity().getContent();
+	    	        responseReader = new BufferedReader(new InputStreamReader(responseStream));
+	    		   	String responseLine = "";
+	    	        while ((responseLine = responseReader.readLine())!=null) {
+                        if(responseLine.contains("Auth")) googleKey=responseLine.split("=")[1];
+                    }
+	            } catch (IllegalStateException e) {
+	    		    e.printStackTrace();
+	            } catch (IOException e) {
+    	    		e.printStackTrace();
+	            }
+	    }
+		Log.d("main", "authentiated");
+    	}catch (Exception e){
+    		e.printStackTrace();	
+    	}
+        return googleKey;
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
